2cb936f5ab4d745421a2849ce45140570fe46f71
Hayden White <hayden@revyl.ai>
2025-08-20 16:30:43 -0700

    fix: Update Kotlin files for React Native 0.72 compatibility
    - Removed references to DefaultReactActivityDelegate and new architecture APIs
    - Simplified MainActivity to only extend ReactActivity
    - Updated MainApplication to use RN 0.72 compatible APIs
    - Removed ReactHost and new architecture initialization code

--- apps/rn-android-minimal/android/app/src/main/java/com/revylrnminimal/MainActivity.kt
+++ apps/rn-android-minimal/android/app/src/main/java/com/revylrnminimal/MainActivity.kt
@@ -1,9 +1,6 @@
 package com.revylrnminimal
 
 import com.facebook.react.ReactActivity
-import com.facebook.react.ReactActivityDelegate
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
-import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
 
@@ -12,11 +9,4 @@ class MainActivity : ReactActivity() {
      * rendering of the component.
      */
     override fun getMainComponentName(): String = "RevylRNMinimal"
-
-    /**
-     * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-     * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
-     */
-    override fun createReactActivityDelegate(): ReactActivityDelegate =
-        DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
 }
